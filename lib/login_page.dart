e1c116ef21cfdb042de7438c352454fcd65958cb
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-10-28 12:04:54 +0530

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  
+  TextEditingController usernameController= TextEditingController();
+  TextEditingController passwordController= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Color.fromARGB(255, 91, 184, 147),
+              Color.fromARGB(255, 225, 192, 71)
+            ])),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 170),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.person,
+                  size: 120,
+                  color: Colors.white,
+                  
+                  
+                ),
+              ),
+            ),
+             const SizedBox(height: 20,),
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller:usernameController ,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                hintStyle: const TextStyle(color: Colors.white),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18,),
+                 borderSide: const BorderSide(color: Colors.white,width: 2.0),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10,),
+                  TextField(
+                              style: const TextStyle(color: Colors.white),
+                              controller:passwordController ,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                hintStyle: const TextStyle(color: Colors.white),
+                                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18,),
+                                 borderSide: const BorderSide(color: Colors.white,width: 2.0),
+                                ),
+                              ),
+                              
+                            ),
+           const SizedBox(height: 20,),
+            ElevatedButton(
+          
+              onPressed: (){},
+               
+                 style: ElevatedButton.styleFrom(
+                  
+                  shape: const StadiumBorder(),
+                  
+                  foregroundColor: Colors.blue,
+                  backgroundColor: Colors.white,
+                  ),
+              
+               child: const SizedBox(width: double.infinity,   
+                child: Center(child: Text('sign in',style: TextStyle(fontSize: 20),))),
+               ),
+               
+          ],
+        ),
+      ),
+    );
+  }
+}
